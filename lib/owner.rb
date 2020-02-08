a25c03ce7678c93939871c2e1cc83136e70c58a0
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-08 19:35:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,41 @@
+require('pry')
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all.push(self)
+  end
+    
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats 
+    Cat.all.select {|cat| cat.owner == self}
+  end
+    
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(name)
+    my_cat = Cat.all.find{|cat| cat.name == name}
+    my_cat.owner = self
+    binding.pry
+  end
 end
