2c0e0c34fb33086e7d55a2e60358ef4f96ec45bc
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-08 20:03:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,23 @@
 require('pry')
+class Cat
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    @@all.push(self)
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+end
+
 class Owner
   attr_reader :name, :species
   @@all = []
